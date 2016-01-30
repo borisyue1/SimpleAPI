996fc69d0bc177fa553f64f9e3fc75b3bb8073ba
Boris Yue <borisyue@yahoo.com>
2016-01-30 11:48:11 -0800

    test

--- SimpleAPI/ViewController.swift
+++ SimpleAPI/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let endpoint: String = "http://jsonplaceholder.typicode.com/posts/1"
+        guard let url = NSURL(string: endpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        let urlRequest = NSURLRequest(URL: url)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(urlRequest, completionHandler: {(data, response, error) in
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            guard error == nil else {
+                print("error calling GET on /posts/1")
+                print(error)
+                return
+            }
+            let post: NSDictionary
+            do {
+                post = try NSJSONSerialization.JSONObjectWithData(responseData,
+                    options: []) as! NSDictionary
+            } catch  {
+                print("error trying to convert data to JSON")
+                return
+            }
+        print("The post is: " + post.description)
+        if let postTitle = post["title"] as? String {
+            print("The title is: " + postTitle)
+        }
+        })
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
